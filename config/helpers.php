0b36c76eb2ff959fab4f61f4075bff80df53c989
Januar Atmaja <januarvino79@gmail.com>
2026-06-18 03:50:16 +0700

    Adjust Reality Club frame slots to cover gaps

--- config/helpers.php
+++ config/helpers.php
@@ -72,8 +72,8 @@ function get_frames(): array
                 ['x' => 3.96, 'y' => 51.58, 'width' => 92.5, 'height' => 22.16],
             ],
             'reality-club-presents...' => [
-                ['x' => 10.0, 'y' => 10.0, 'width' => 80.0, 'height' => 35.0],
-                ['x' => 10.0, 'y' => 55.0, 'width' => 80.0, 'height' => 35.0],
+                ['x' => 12.0, 'y' => 11.0, 'width' => 76.0, 'height' => 38.0],
+                ['x' => 12.0, 'y' => 51.0, 'width' => 76.0, 'height' => 38.0],
             ],
             'music-player' => [
                 ['x' => 10, 'y' => 4, 'width' => 80, 'height' => 22],
